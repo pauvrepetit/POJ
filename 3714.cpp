0eefb8d86db8710149944ddba955a437a108ced3
Hu Ao <hu_ao@icloud.com>
2019-11-08 17:32:27 +0800

    3714

--- 3714.cpp
+++ 3714.cpp
@@ -1,11 +1,12 @@
 #include <algorithm>
 #include <cmath>
+#include <cstdio>
 #include <cstring>
 #include <iomanip>
 #include <iostream>
-#include <vector>
 
 #define INF 1e308
+#define INT32_MAX (2147483647)
 
 using std::cin;
 using std::cout;
@@ -14,30 +15,20 @@ using std::ios;
 using std::setiosflags;
 using std::setprecision;
 using std::sort;
-using std::vector;
 
-class Point {
-  public:
+struct Point {
     int x;
     int y;
     int type;
-
-    Point() {}
-
-    Point(int x_, int y_, int type_) {
-        this->x = x_;
-        this->y = y_;
-        this->type = type_;
-    }
 };
 
 bool Xsort(Point a, Point b) { return (a.x < b.x); }
 bool Ysort(Point a, Point b) { return (a.y < b.y); }
 
-double getMinDistance(vector<Point> pointListX, vector<Point> pointListY, int n);
-double getCrossDistance(vector<Point> leftList, vector<Point> rightList, double leftRightMinDistance, int leftSize, int rightSize);
+double getMinDistance(Point *pointListX, Point *pointListY, int n);
+double getCrossDistance(Point *crossPointList, int crossPointCount, double leftRightMinDistance);
 double getDistance(Point p1, Point p2);
-double getLineDistance(vector<Point> linePointList, int n);
+double getLineDistance(Point *linePointList, int n);
 
 int main() {
     int t;
@@ -47,34 +38,37 @@ int main() {
         cin >> n;
         int x;
         int y;
-        vector<Point> pointListX;
-        vector<Point> pointListY;
+        Point *pointListX = new Point[n * 2];
+        Point *pointListY = new Point[n * 2];
         Point temp;
         for (int j = 0; j < n; j++) {
-            cin >> x >> y;
+            scanf("%d%d", &x, &y);
             temp.x = x;
             temp.y = y;
             temp.type = 0;
-            pointListX.push_back(temp);
+            pointListX[j] = temp;
         }
         for (int j = 0; j < n; j++) {
-            cin >> x >> y;
+            scanf("%d%d", &x, &y);
             temp.x = x;
             temp.y = y;
             temp.type = 1;
-            pointListX.push_back(temp);
+            pointListX[j + n] = temp;
         }
-        pointListY = pointListX;
-        sort(pointListX.begin(), pointListX.end(), Xsort);
-        sort(pointListY.begin(), pointListY.end(), Ysort);
+        memcpy(pointListY, pointListX, sizeof(Point) * n * 2);
+        sort(pointListX, pointListX + n * 2, Xsort);
+        sort(pointListY, pointListY + n * 2, Ysort);
 
         cout << setiosflags(ios::fixed) << setprecision(3) << getMinDistance(pointListX, pointListY, n * 2) << endl;
+
+        delete[] pointListX;
+        delete[] pointListY;
     }
 
     return 0;
 }
 
-double getMinDistance(vector<Point> pointListX, vector<Point> pointListY, int n) {
+double getMinDistance(Point *pointListX, Point *pointListY, int n) {
     if (n == 0 || n == 1) {
         return INF;
     }
@@ -82,77 +76,90 @@ double getMinDistance(vector<Point> pointListX, vector<Point> pointListY, int n)
         return getDistance(pointListX[0], pointListX[1]);
     }
 
-    vector<Point> leftPointListY(n);
-    vector<Point> rightPointListY(n);
-    vector<Point> leftPointListX(n);
-    vector<Point> rightPointListX(n);
+    Point *leftPointListY = new Point[n];
+    Point *rightPointListY = new Point[n];
+    Point *midPointListY = new Point[n];
 
     Point minPoint = pointListX[n / 2];
+    int mid = minPoint.x;
     int flag = 0;
     int firstType = pointListY[0].type;
     int leftY = 0;
     int rightY = 0;
-    int leftX = 0;
-    int rightX = 0;
+    int midY = 0;
     for (int i = 0; i < n; i++) {
         if (firstType != pointListY[i].type) {
             flag = 1;
         }
-        if (pointListY[i].x < minPoint.x) {
+        if (pointListY[i].x < mid) {
             leftPointListY[leftY++] = pointListY[i];
-        } else {
+        } else if (pointListY[i].x > mid) {
             rightPointListY[rightY++] = pointListY[i];
-        }
-        if (pointListX[i].x < minPoint.x) {
-            leftPointListX[leftX++] = pointListX[i];
         } else {
-            rightPointListX[rightX++] = pointListX[i];
+            midPointListY[midY++] = pointListY[i];
         }
     }
     if (flag == 0) {
         return INF;
     }
-
     double leftDistance;
     double rightDistance;
+    double midDistance;
 
-    if (leftY == 0 && rightY >= 2) {
-        leftDistance = INF;
-        rightDistance = getLineDistance(rightPointListY, rightY);
-    } else {
-        leftDistance = getMinDistance(leftPointListX, leftPointListY, leftY);
-        rightDistance = getMinDistance(rightPointListX, rightPointListY, rightY);
-    }
+    leftDistance = getMinDistance(pointListX, leftPointListY, leftY);
+    rightDistance = getMinDistance(pointListX + leftY + midY, rightPointListY, rightY);
+    midDistance = getLineDistance(midPointListY, midY);
 
     double minLeftRightDistance;
-    if (leftDistance > rightDistance) {
+    if (leftDistance > rightDistance && midDistance > rightDistance) {
         minLeftRightDistance = rightDistance;
-    } else {
+    } else if (rightDistance > leftDistance && midDistance > leftDistance) {
         minLeftRightDistance = leftDistance;
+    } else {
+        minLeftRightDistance = midDistance;
     }
 
-    double mid = minPoint.x;
-    double left = mid - minLeftRightDistance;
-    double right = mid + minLeftRightDistance;
+    double leftMax = mid - minLeftRightDistance;
+    double rightMax = mid + minLeftRightDistance;
 
-    vector<Point> leftLessPoint(leftY);
-    vector<Point> rightLessPoint(rightY);
+    Point *crossPointList = new Point[n];
+    int crossPointCount = 0;
 
-    leftY = 0;
-    rightY = 0;
+    leftPointListY[leftY].y = INT32_MAX;
+    rightPointListY[rightY].y = INT32_MAX;
+    midPointListY[midY].y = INT32_MAX;
 
-    for (int i = 0; i < leftX; i++) {
-        if (leftPointListY[i].x > left) {
-            leftLessPoint[leftY++] = leftPointListY[i];
+    int leftCross = 0;
+    int rightCross = 0;
+    int midCross = 0;
+
+    while (1) {
+        if (leftPointListY[leftCross].y <= rightPointListY[rightCross].y && leftPointListY[leftCross].y <= midPointListY[midCross].y && leftCross < leftY) {
+            if (leftPointListY[leftCross].x >= leftMax) {
+                crossPointList[crossPointCount++] = leftPointListY[leftCross];
+            }
+            leftCross++;
+        } else if (rightPointListY[rightCross].y <= leftPointListY[leftCross].y && rightPointListY[rightCross].y <= midPointListY[midCross].y &&
+                   rightCross < rightY) {
+            if (rightPointListY[rightCross].x <= rightMax) {
+                crossPointList[crossPointCount++] = rightPointListY[rightCross];
+            }
+            rightCross++;
+        } else if (midPointListY[midCross].y <= leftPointListY[leftCross].y && midPointListY[midCross].y <= rightPointListY[rightCross].y && midCross < midY) {
+            crossPointList[crossPointCount++] = midPointListY[midCross++];
+        }
+
+        if (leftCross == leftY && rightCross == rightY && midCross == midY) {
+            break;
         }
     }
-    for (int i = 0; i < rightX; i++) {
-        if (rightPointListY[i].x <= right) {
-            rightLessPoint[rightY++] = rightPointListY[i];
-        }
-    }
 
-    double crossDistance = getCrossDistance(leftLessPoint, rightLessPoint, minLeftRightDistance, leftY, rightY);
+    double crossDistance = getCrossDistance(crossPointList, crossPointCount, minLeftRightDistance);
+
+    delete[] leftPointListY;
+    delete[] rightPointListY;
+    delete[] crossPointList;
+
     if (crossDistance > minLeftRightDistance) {
         return minLeftRightDistance;
     } else {
@@ -160,32 +167,20 @@ double getMinDistance(vector<Point> pointListX, vector<Point> pointListY, int n)
     }
 }
 
-double getCrossDistance(vector<Point> left, vector<Point> right, double leftRightMinDistance, int leftSize, int rightSize) {
-    int leftCount = 0;
-    int rightCount = 0;
+double getCrossDistance(Point *crossPointList, int crossPointCount, double leftRightMinDistance) {
     double minDistance = INF;
-    double tempDistance;
-    int length = leftSize;
-    int rightMax = rightSize;
-    if (length == 0 || rightMax == 0) {
-        return minDistance;
-    }
-
-    for (leftCount = 0; leftCount < length; leftCount++) {
-        while (right[rightCount].y < left[leftCount].y - leftRightMinDistance) {
-            rightCount++;
-            if (rightCount >= rightMax) {
-                return minDistance;
-            }
-        }
-        for (int i = 0; (i < 16) && (rightCount + i < rightMax); i++) {
-            if ((tempDistance = getDistance(left[leftCount], right[rightCount + i])) < minDistance) {
-                minDistance = tempDistance;
+    double temp;
+    for (int i = 0; i < crossPointCount; i++) {
+        for (int j = i + 1; (j < i + 10) && (j < crossPointCount); j++) {
+            temp = getDistance(crossPointList[i], crossPointList[j]);
+            if (temp < minDistance) {
+                minDistance = temp;
             }
         }
     }
     return minDistance;
 }
+
 
 double getDistance(Point p1, Point p2) {
     if (p1.type == p2.type) {
@@ -196,12 +191,17 @@ double getDistance(Point p1, Point p2) {
     return sqrt(x * x + y * y);
 }
 
-double getLineDistance(vector<Point> linePointList, int n) {
+double getLineDistance(Point *linePointList, int n) {
     double minDistance = INF;
-    double tempDistance;
+    double temp;
     for (int i = 0; i < n - 1; i++) {
-        if ((tempDistance = getDistance(linePointList[i], linePointList[i + 1])) < minDistance) {
-            minDistance = tempDistance;
+        if (linePointList[i + 1].type == linePointList[i].type) {
+            temp = INF;
+        } else {
+            temp = linePointList[i + 1].y - linePointList[i].y;
+        }
+        if (temp < minDistance) {
+            minDistance = temp;
         }
     }
     return minDistance;
